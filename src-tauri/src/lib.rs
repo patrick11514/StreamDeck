59ee1c575a6311ae1584cc1801bf8f6b6a59dedf
Patrik Mintěl <ja@patrick115.eu>
2025-08-25 21:37:49 +0200

    Initialized tailwind + shadcn-svelte + basic listing streamdecks

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,34 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use hidapi::HidApi;
+
+const ELGATO_VID: u16 = 0x0fd9;
+
+#[derive(serde::Serialize)]
+struct DeckInfo {
+    vid: u16,
+    pid: u16,
+    serial: Option<String>,
+    product: Option<String>,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_streamdecks() -> Vec<DeckInfo> {
+    let api = HidApi::new().unwrap();
+    api.device_list()
+        .filter(|dev| dev.vendor_id() == ELGATO_VID)
+        .map(|dev| DeckInfo {
+            vid: dev.vendor_id(),
+            pid: dev.product_id(),
+            serial: dev.serial_number().map(|s| s.to_string()),
+            product: dev.product_string().map(|s| s.to_string()),
+        })
+        .collect()
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_streamdecks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import "../app.css"
+    import "../fonts.css"
+
+    import type { LayoutProps } from "./$types"
+
+    const { children }: LayoutProps = $props()
+</script>
+
+<main
+    class="min-h-screen font-loto gap-2 h-full w-full flex flex-col items-center justify-center bg-background text-foreground dark overflow-x-hidden"
+>
+    {@render children()}
+</main>
